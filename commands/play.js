af2582d272dd8dcbae55e61ae67df33d58ac1d82
Mariano Gonzalez <mariano722003@gmail.com>
2022-11-20 03:41:24 -0300

    Comando Play añadido, aunque apenas funcional

--- commands/play.js
+++ commands/play.js
@@ -1,14 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus} = require("@discordjs/voice");
-
+const { SlashCommandBuilder } = require("discord.js");
+const ytdl = require('ytdl-core');
+const ytsr = require('ytsr');
+const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Reproduce una cancion de YouTube'),
+        .setDescription('Reproduce una cancion de YouTube')
+        .addStringOption(option =>
+            option
+                .setName("cancion")
+                .setDescription("La cancion a reproducir.")
+                .setRequired(true)
+            ),
     async execute (interaction)
     {
-        const resource = createAudioResource("C:\\Users\\PC\\Downloads\\test.mp3")
+        const ytVideo = await searchYoutubeVideo(interaction.options.getString('cancion'));
+        const resource = createAudioResource(getYoutubeVideoStream(ytVideo.url));
+        console.log(ytVideo);
         const player = createAudioPlayer();
         const connection = joinVoiceChannel({
             channelId: interaction.member.voice.channel.id,
@@ -17,10 +27,19 @@ module.exports = {
         })
         player.play(resource);
         connection.subscribe(player);
-        interaction.reply("Exito!");
+        interaction.reply("Reproduciendo: " + ytVideo.title);
     },
 };
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+ async function searchYoutubeVideo (searchTerm) {
+    const search = await ytsr(searchTerm, {limit: 1} );
+    return search.items[0];
+ }
+
+function getYoutubeVideoStream (url) {
+    return ytdl(url, {filter: 'audioonly', dlChunkSize: 0});
 }
